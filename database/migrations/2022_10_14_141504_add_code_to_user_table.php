5aa175cde0d8505fc9e8382410b35e0049d5cbec
Baldomero Cho <don.baldo@hotmail.com>
2022-10-14 15:05:50 -0600

    seeders and readme steps

--- database/migrations/2022_10_14_141504_add_code_to_user_table.php
+++ database/migrations/2022_10_14_141504_add_code_to_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('code',6);
+            $table->integer('code')->nullable();
         });
     }
 
--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cine\Application\City;
+use App\Models\Cine\Application\Country;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
 class CitySeeder extends Seeder
 {
@@ -14,6 +17,20 @@ class CitySeeder extends Seeder
      */
     public function run()
     {
-        //
+        try{
+            $country = Country::firstOrFail();
+            $cities = [
+                [
+                    'name'=> 'Ciudad Guatemala',
+                    'country_id'=> $country->id
+                ]
+            ];
+        } catch (\Exception $e){
+            Log::error($e->getMessage());
+        }
+        foreach ($cities as $city)
+        {
+            City::create($city);
+        }
     }
 }
