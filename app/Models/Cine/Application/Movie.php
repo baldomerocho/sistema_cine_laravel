920c2683fed8659dff6a28efd20bb6f2d6ec6504
Baldomero Cho <don.baldo@hotmail.com>
2022-10-14 13:57:48 -0600

    movie model update

--- app/Models/Cine/Application/Movie.php
+++ app/Models/Cine/Application/Movie.php
@@ -4,8 +4,34 @@ namespace App\Models\Cine\Application;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Movie extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'tagline',
+        'original_title',
+        'video',
+        'status',
+        'language',
+        'poster_path',
+        'backdrop_path',
+        'imdb_id',
+        'tmdb_id',
+        'adult'
+    ];
+
+    public function genders()
+    {
+        return $this->belongsToMany(Gender::class);
+    }
+
+    public function rooms()
+    {
+        return $this->belongsToMany(Room::class);
+    }
 }
