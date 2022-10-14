288c266e5ffab21fb9a6cf84bcaf1dde0bd40c1a
Baldomero Cho <don.baldo@hotmail.com>
2022-10-14 13:46:00 -0600

    movies migration

---
+++ app/Http/Requests/UpdateMovieRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateMovieRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title'=>'required|string|max:255',
+            'tagline'=>'required|string|max:255',
+            'original_title'=>'required|string|max:255',
+            'video'=>'string|max:2048',
+            'status'=>'string|max:50',
+            'language'=>'string|max:10',
+            'poster_path'=>'required|string|max:100',
+            'backdrop_path'=>'required|string|max:100',
+            'imdb_id'=>'required|integer|unique:movies',
+            'tmdb_id'=>'required|integer|unique:movies',
+            'adult'=>'required|boolean'
+        ];
+    }
+}
---
+++ app/Models/Cine/Application/Movie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Cine\Application;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+}
