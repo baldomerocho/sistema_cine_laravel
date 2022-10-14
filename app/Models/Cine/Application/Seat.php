08c0a7dbc76d470ffa3cf0c8fef63db0cc9bcdb9
Baldomero Cho <don.baldo@hotmail.com>
2022-10-14 13:28:45 -0600

    seats migration

---
+++ app/Models/Cine/Application/Seat.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Cine\Application;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Seat extends Model
+{
+    use HasFactory;
+}
